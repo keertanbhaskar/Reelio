e5bc33c52c977459c60f1692e89b9368501c2fee
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-06-28 23:33:40 +0530

    login page background set

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <img
+        src={assets.bgImage}
+        alt=""
+        className="absolute top-0 left-0 -z-1 w-full h-full object-cover"
+      />
+    </div>
+  );
 }
 
 export default Login
