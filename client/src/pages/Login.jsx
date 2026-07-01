92cbdd871ab807e68e229e8eacda04e5b0b31487
Keertana Bhaskar <keertanbhaskar@gmail.com>
2026-07-01 23:11:25 +0530

    Added the logo and the users at bottom of page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets } from "../assets/assets";
+import { Star } from 'lucide-react';
 
 const Login = () => {
   return (
@@ -7,8 +8,25 @@ const Login = () => {
       <img
         src={assets.bgImage}
         alt=""
-        className="absolute top-0 left-0 -z-1 w-full h-full object-cover"
+        className="absolute top-0 left-0 -z-1  w-full h-full object-cover"
       />
+      {/* left side: Branding */}
+      <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40'>
+        <img src={assets.logo} alt="" className='h-12 object-contain' />
+        <div>
+          <div className='flex items-center gap-3 mb-4 max-md:mt-10'>
+            <img src={assets.group_users}  alt="" className='h-8 md:h-10' />
+            <div>
+              <div className='flex'>
+                {Array(5).fill(0).map((_,i)=>(<Star key={i} 
+                className='size-4 md:size-4.5 text-transparent
+                fill-amber-500' />))}
+              </div>
+              <p>Used by developers</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
